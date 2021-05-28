3dfdf2e7a6325647c87a1b64c1dc367a4708bf07
Jader Leite <jaderstz16@gmail.com>
2021-05-27 22:58:47 -0300

    final aula 3

--- 6-listas.js
+++ 6-listas.js
@@ -1,7 +1,7 @@
-console.log(`Trabalhando com listas`);           //BURRICE                 
+console.log(`Trabalhando com listas`);                           
 // const salvador = `Salvador`;                   //BURRICE
-// const saoPaulo = `São Paulo`;                   //BURRICE
-// const rioDeJaneiro = `Rio de Janeiro`;            //BURRICE       
+// const saoPaulo = `São Paulo`;                  //BURRICE
+// const rioDeJaneiro = `Rio de Janeiro`;         //BURRICE       
 
 
 const listaDeDestinos = new Array(           //vetor
@@ -10,7 +10,12 @@ const listaDeDestinos = new Array(           //vetor
     `Rio de Janeiro`
 )
 
-listaDeDestinos.push(`Curitiba`) //push adiciona um item na lista
+listaDeDestinos.push(`Curitiba`) //push adiciona um item dinamicamente na lista
 console.log(`Destinos Possíveis`);                  
 console.log(listaDeDestinos);    
 
+
+listaDeDestinos.splice(1,1); // splice remove um item dinamicamente do vetor. aqui o vetor tbm começa no 0
+console.log(listaDeDestinos);  
+
+console.log(listaDeDestinos[0], listaDeDestinos[1]);
