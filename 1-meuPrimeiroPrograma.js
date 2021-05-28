a0d181595a00f9cb9611e158250da7b9b937c300
Jader Leite <jaderstz16@gmail.com>
2021-05-27 21:28:09 -0300

    first commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Meu primeiro programa JavaScript");
+const idade = 26;
+console.log(idade);
+console.log(idade/2);
+console.log(idade+2);
+console.log(idade*2);
+
+const anjo = "beijo";
+
+console.log(anjo);
+
